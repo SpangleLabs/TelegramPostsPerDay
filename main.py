5371657d4c12f47489e6c14abe96810c51c50a9a
Joshua Coales <joshua@coales.co.uk>
2020-03-07 13:43:20 +0000

    Adding a total message count, and a progress bar

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import json
 from collections import defaultdict
 
 import telethon
+from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.tl.types.messages import Messages
+from tqdm import tqdm
 
 config = {
     "api_id": "",
@@ -16,13 +19,37 @@ async def iter_channel_messages(client, channel_handle: str):
         yield message
 
 
+async def get_message_count(client, channel_handle):
+    chat_entity = await client.get_entity(channel_handle)
+    get_history = GetHistoryRequest(
+        peer=chat_entity,
+        offset_id=0,
+        offset_date=None,
+        add_offset=0,
+        limit=1,
+        max_id=0,
+        min_id=0,
+        hash=0
+    )
+    history = await client(get_history)
+    if isinstance(history, Messages):
+        count = len(history.messages)
+    else:
+        count = history.count
+    return count
+
+
 async def parse_messages(client, chat_handle):
     data = {
         "by_date": defaultdict(lambda: 0)
     }
-    async for message in iter_channel_messages(client, chat_handle):
-        date = message.date.date().isoformat()
-        data["by_date"][date] += 1
+    count = await get_message_count(client, chat_handle)
+    data["count"] = count
+    with tqdm(total=count) as bar:
+        async for message in iter_channel_messages(client, chat_handle):
+            date = message.date.date().isoformat()
+            data["by_date"][date] += 1
+            bar.update(1)
     return data
 
 
