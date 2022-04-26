7368fe4af177c94fb9a5946f2e607488f1aa25d4
Joshua Coales <joshua@coales.co.uk>
2022-04-26 09:37:40 +0100

    Implementing database methods in Database class, and methods to parse LogEntry from db row, and convert it back to one again

--- convert_to_irssi_logs.py
+++ convert_to_irssi_logs.py
@@ -46,6 +46,102 @@ class Database:
         )
         self.metadata.create_all(self.engine)
 
+    def insert_log_entries(self, chat_handle: str, log_entries: List["LogEntry"]):
+        query = sqlalchemy.insert(self.log_entries)
+        values_list = [
+            log_entry.to_row(chat_handle) for log_entry in log_entries
+        ]
+        self.conn.execute(query, values_list)
+
+    def list_log_dates(self, chat_handle: str) -> List[datetime.date]:
+        cols = [sqlalchemy.cast(self.log_entries.columns.datetime, sqlalchemy.Date)]
+        if self.engine.url.drivername == "sqlite":
+            cols = [sqlalchemy.func.DATE(self.log_entries.columns.datetime).label("datetime")]
+        query = sqlalchemy.select(
+            cols
+        ).distinct(
+        ).where(
+            self.log_entries.columns.chat_handle == chat_handle
+        ).order_by(
+            sqlalchemy.asc(self.log_entries.columns.datetime)
+        )
+        result = self.conn.execute(query)
+        rows = result.fetchall()
+        if self.engine.url.drivername == "sqlite":
+            return [
+                datetime.date.fromisoformat(row.datetime) for row in rows
+            ]
+        return [
+            row.datetime for row in rows
+        ]
+
+    def list_log_entries(self, chat_handle: str, log_date: Optional[datetime.date]):
+        conditions = [
+            self.log_entries.columns.chat_handle == chat_handle
+        ]
+        if log_date:
+            start_datetime = datetime.datetime.combine(log_date, datetime.time(0, 0, 0))
+            end_datetime = start_datetime + datetime.timedelta(days=1)
+            conditions.extend([
+                self.log_entries.columns.datetime >= start_datetime,
+                self.log_entries.columns.datetime < end_datetime
+            ])
+        query = sqlalchemy.select(
+            self.log_entries.columns
+        ).where(
+            sqlalchemy.and_(*conditions)
+        ).order_by(
+            sqlalchemy.asc(self.log_entries.columns.datetime)
+        )
+        result = self.conn.execute(query)
+        return [
+            LogEntry.from_row(row)
+            for row in result.fetchall()
+        ]
+
+    def list_user_ids(self, chat_handle: str) -> Set[int]:
+        query = sqlalchemy.select(
+            self.log_entries.columns.user_id
+        ).distinct(
+            self.log_entries.columns.user_id
+        ).where(
+            self.log_entries.columns.chat_handle == chat_handle
+        )
+        result = self.conn.execute(query)
+        return set([row.user_id for row in result.fetchall()])
+
+    def update_chat_log(self, chat_handle: str, last_message_id: Optional[int]):
+        query = sqlalchemy.update(
+            self.chat_logs
+        ).values(
+            last_message_id=last_message_id
+        ).where(
+            self.chat_logs.columns.chat_handle == chat_handle
+        )
+        self.conn.execute(query)
+
+    def create_chat_log(self, chat_handle: str) -> None:
+        query = sqlalchemy.insert(
+            self.chat_logs
+        ).values(
+            chat_handle=chat_handle,
+            last_message_id=None
+        )
+        self.conn.execute(query)
+
+    def get_chat_log(self, chat_handle: str) -> "ChatLog":
+        query = sqlalchemy.select(
+            self.chat_logs.columns.last_message_id
+        ).where(
+            self.chat_logs.columns.chat_handle == chat_handle
+        )
+        result = self.conn.execute(query)
+        row = result.fetchone()
+        if row is None:
+            self.create_chat_log(chat_handle)
+            return ChatLog(chat_handle, self)
+        return ChatLog(chat_handle, self, last_message_id=row.last_message_id)
+
 
 class DataStore:
 
@@ -282,6 +378,24 @@ class LogEntry:
             data["entry_type"],
             data["user_id"],
             data["text"]
+        )
+
+    def to_row(self, chat_handle: str) -> Dict:
+        return {
+            "chat_handle": chat_handle,
+            "datetime": self.log_datetime,
+            "entry_type": self.log_type,
+            "user_id": self.user_id,
+            "text": self.text
+        }
+
+    @staticmethod
+    def from_row(row) -> "LogEntry":
+        return LogEntry(
+            row.datetime,
+            row.entry_type,
+            row.user_id,
+            row.text
         )
 
     @classmethod
